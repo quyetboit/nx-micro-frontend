300c5235d09452af5317947def4139044fe878ad
quyetboit <quyetboit@gmail.com>
2023-08-21 22:47:09 +0700

    generate app remote by @nx/angular

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'home',
+    loadChildren: () => import('home/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
